11579e4306c0a958e87fd6f12825e96704effebb
Ashish Rathi <rathiashish4321@gmail.com>
2025-11-19 13:40:44 +0530

    responsiveness of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        <p className="text-center text-md text-gray-500 fixed bottom-1 left-0 right-0">
-            &copy; 2023 Dice Game - By <span className='font-bold'>Ashish Rathi</span>. All rights reserved.
-        </p>
-    </div>
+    <footer className="fixed bottom-0 left-0 w-full bg-white py-2 md:py-3">
+      <p className="text-center text-sm md:text-md text-gray-500">
+        &copy; 2025 Dice Game - By{' '}
+        <span className="font-bold">Ashish Rathi</span>. All rights reserved.
+      </p>
+    </footer>
   )
 }
 
